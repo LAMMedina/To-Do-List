e653b9a3ae375f49afe81cc429f3fc83308b6c59
Luis Mujica <luismujicamedina@gmail.com>
2024-11-28 15:06:38 -0500

    Configura conexión con MongoDB Atlas

---
+++ backend/models/Task.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const TaskSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  description: {
+    type: String,
+    trim: true
+  },
+  status: {
+    type: String,
+    enum: ['Pendiente', 'En Proceso', 'Completada'],
+    default: 'Pendiente'
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model('Task', TaskSchema);
